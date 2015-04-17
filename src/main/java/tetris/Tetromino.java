2c98cba5615ba7aa2ae71d26c1abd4d2a78cfcbb
Juha Syrjälä <juha.syrjala@gmail.com>
2015-04-17 11:17:02 +0300

    tetromino tests starts

---
+++ src/main/java/tetris/Tetromino.java
@@ -0,0 +1,20 @@
+// Copyright (c) 2008-2015  Esko Luontola <www.orfjackal.net>
+// You may use and modify this source code freely for personal non-commercial use.
+// This source code may NOT be used as course material without prior written agreement.
+
+package tetris;
+
+/**
+ * Created by jsyrjala on 4/17/15.
+ */
+public class Tetromino extends Piece {
+    public static final Tetromino T_SHAPE = new Tetromino("" +
+            "...\n" +
+            "...\n" +
+            "...\n"
+    );
+
+    public Tetromino(String blockData) {
+        super(blockData);
+    }
+}
